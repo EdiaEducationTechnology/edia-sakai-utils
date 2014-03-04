e4ddc9bec54a5ca52dc03e767c80ec5860768a59
Roland Groen <roland@edia.nl>
2014-03-04 21:56:18 +0000

    Fixing broken formatted text tets

--- utils/src/test/nl/edia/masla/sakai/tag/TrimFormattedTextTagTest.java
+++ utils/src/test/nl/edia/masla/sakai/tag/TrimFormattedTextTagTest.java
@@ -4,9 +4,14 @@ import javax.servlet.jsp.JspException;
 
 import junit.framework.TestCase;
 
+import org.sakaiproject.component.cover.ComponentManager;
+import org.sakaiproject.util.impl.FormattedTextImpl;
+
 public class TrimFormattedTextTagTest extends TestCase {
 
 	public void testGetTextFormatted() throws JspException {
+		ComponentManager.testingMode = true;
+		ComponentManager.loadComponent(org.sakaiproject.util.api.FormattedText.class,  new FormattedTextImpl());
 		String myString = "formatted";
 		String myTextFormatted = new TrimFormattedTextTag().getTextFormatted(myString);
 		assertEquals(myString, myTextFormatted);
