267f6f4ae7a349572af2268202411fd59a8916de
joris <joris@edia.nl>
2016-07-18 16:51:32 +0200

    NTTOEK-75 Made the saki-kernel dependency from the root pom a test scope. The FormattedText is now requested by the ComponentManager

--- utils/src/test/nl/edia/masla/sakai/tag/TrimFormattedTextTagTest.java
+++ utils/src/test/nl/edia/masla/sakai/tag/TrimFormattedTextTagTest.java
@@ -3,12 +3,21 @@ package nl.edia.masla.sakai.tag;
 import javax.servlet.jsp.JspException;
 
 import junit.framework.TestCase;
+import org.sakaiproject.util.api.FormattedText;
+import org.sakaiproject.util.impl.FormattedTextImpl;
 
 public class TrimFormattedTextTagTest extends TestCase {
 
 	public void testGetTextFormatted() throws JspException {
 		String myString = "formatted";
-		String myTextFormatted = new TrimFormattedTextTag().getTextFormatted(myString);
+
+		TrimFormattedTextTag formattedTextTag = new TrimFormattedTextTag(){
+			protected FormattedText getFormattedText() {
+				return new FormattedTextImpl();
+			}
+		};
+
+		String myTextFormatted = formattedTextTag.getTextFormatted(myString);
 		assertEquals(myString, myTextFormatted);
 	}
 
